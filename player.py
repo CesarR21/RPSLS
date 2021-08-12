03308245ea2103d2ae9a2df57455f50f9fe6682d
Treveon Edwards <fasuke322@gmail.com>
2021-08-12 17:13:56 -0400

    created player class and children

---
+++ player.py
@@ -0,0 +1,11 @@
+class Player:
+    def __init__(self):
+        self.point=""
+        self.score=""
+        self.geastures=["rk","pr","sr","lz","sp"]
+ 
+    def move(self):
+        pass
+
+    # def choose_gesture(self):
+    # chosen_gesture= input    
